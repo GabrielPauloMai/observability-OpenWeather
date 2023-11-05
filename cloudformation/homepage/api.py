ae195da7c0c10742d8c1c17a0c809e6f23abce47
Gabriel Mai <gabrielmai1663@gmail.com>
2023-11-05 11:35:22 -0300

    features in prometheus

--- cloudformation/homepage/api.py
+++ cloudformation/homepage/api.py
@@ -1,4 +1,5 @@
 import requests
+from prometheus_client import Counter
 
 class OpenWeatherAPI:
     
@@ -6,14 +7,22 @@ class OpenWeatherAPI:
         self.base_url_geo = 'http://api.openweathermap.org/geo/1.0/direct'
         self.base_url_weather = 'https://api.openweathermap.org/data/2.5/weather'
         self.api_key = api_key
+
+        # Inicializando as métricas para monitorar solicitações bem-sucedidas e malsucedidas
+        self.successful_requests_counter = Counter('openweatherapi_successful_requests', 'OpenWeatherAPI Successful Requests', ["instance"])
+        self.failed_requests_counter = Counter('openweatherapi_failed_requests', 'OpenWeatherAPI Failed Requests', ["instance"])
     
     def get_city(self, name):
         params = {'q': name, 'limit': 1, 'appid': self.api_key}
         response = requests.get(self.base_url_geo, params=params)
         
         if response.status_code == 200:
+            # Incrementando a métrica de solicitações bem-sucedidas
+            self.successful_requests_counter.labels(instance='openweather-api').inc()
             return response.json()
         else:
+            # Incrementando a métrica de solicitações malsucedidas
+            self.failed_requests_counter.labels(instance='openweather-api').inc()
             raise Exception(f"Localidade {name} não encontrada", response.status_code, response.text)
         
     def get_weather(self, lat, lon):
@@ -21,8 +30,12 @@ class OpenWeatherAPI:
         response = requests.get(self.base_url_weather, params=params)
         
         if response.status_code == 200:
+            # Incrementando a métrica de solicitações bem-sucedidas
+            self.successful_requests_counter.labels(instance='openweather-api', status_code=response.status_code, payload=response.text).inc()
             return response.json()
         else:
+            # Incrementando a métrica de solicitações malsucedidas
+            self.failed_requests_counter.labels(instance='openweather-api', status_code=response.status_code, payload=response.text).inc()
             return None
         
     def get_weather_by_city(self, name):
@@ -32,8 +45,6 @@ class OpenWeatherAPI:
         else:
             print(city_info[0]['lat'], city_info[0]['lon'])
             return self.get_weather(city_info[0]['lat'], city_info[0]['lon'])
-
-
 
 if __name__ == '__main__':
     run = OpenWeatherAPI('c8323e14011f5f3b51fd1235520f0517')
--- cloudformation/homepage/web.py
+++ cloudformation/homepage/web.py
@@ -1,18 +1,24 @@
 import os
 from flask import Flask, render_template, request, redirect
 from api import OpenWeatherAPI
+from prometheus_client import start_http_server, Counter
 
 app = Flask(__name__)
+
+request_counter = Counter('http_requests', 'HTTP request', ["status_code", "instance"])
+api_key_error_counter = Counter('openweather_api_key_error', 'OpenWeather API Key Error', ["instance"])
 
 api_key = 'c8323e14011f5f3b51fd1235520f0517'
 
 if not api_key:
+    api_key_error_counter.labels(instance='flask-app').inc()
     raise ValueError("A chave da API da OpenWeather não foi configurada.")
 
 weather_api = OpenWeatherAPI(api_key)
 
 @app.route('/')
 def index():
+    request_counter.labels(status_code='200', instance='flask-app').inc()
     return render_template('index.html')
 
 @app.route('/weather', methods=['POST'])
@@ -21,6 +27,7 @@ def weather():
     try:
         dados_clima = weather_api.get_weather_by_city(cidade)
     except TypeError as e:
+        
         return render_template('error.html', error=e)
 
 
@@ -34,9 +41,11 @@ def weather():
             'descricao': dados_clima['weather'][0]['description'],
             'icon_code': dados_clima['weather'][0]['icon'],
         }
+        request_counter.labels(status_code='200', instance='flask-app', payload=info_clima).inc()
         return render_template('weather.html', weather=info_clima)
     else:
+        request_counter.labels(status_code='404', instance='flask-app', payload="Falha ao obter dados de clima para {cidade}").inc()
         return render_template('error.html', error=f"Falha ao obter dados de clima para {cidade}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
